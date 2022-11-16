065213e156bc23bd36d133b4be67fff43d7484c5
Weisz Roland <weisz.roland@pohlen-dach.hu>
2022-11-16 21:04:20 +0100

    create a blank image

--- gbfontmaker.py
+++ gbfontmaker.py
@@ -4,15 +4,26 @@
 import pygame as pg
 
 
+FONT_SIZE = 8  # font size in pixels
+LAYOUT = (16, 14)   # layout of the font set per character (width, height)
+BACKGROUND_COLOR = "#e0f8cf"
+
+
 def make_image() -> pg.Surface:
     """Create a pygame.Surface object holding the characters."""
-    return pg.Surface()
+    # steup the canvas
+    canvas_width = FONT_SIZE * LAYOUT[0]
+    canvas_height = FONT_SIZE * LAYOUT[1]
+    background_color = pg.Color(BACKGROUND_COLOR)
+    canvas = pg.Surface((canvas_width, canvas_height))
+    canvas.fill(background_color)
+    return canvas
 
 
-def save2png(image: pg.Surface) -> bool:
+def save2png(image:pg.Surface) -> None:
     """Save the image in .png format."""
-    return False
+    pg.image.save(image, "font.png")
 
 
 if __name__ == "__main__":
-    pass
+    save2png(make_image())
