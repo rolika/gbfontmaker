6ab0486ec4d2ae1eda07b7c91af549f9a08c9e06
Weisz Roland <weisz.roland@pohlen-dach.hu>
2022-11-16 20:12:21 +0100

    initial commit

---
+++ gbfontmaker.py
@@ -0,0 +1,18 @@
+"""Utility to create a font image for GB Studio."""
+
+
+import pygame as pg
+
+
+def make_image() -> pg.Surface:
+    """Create a pygame.Surface object holding the characters."""
+    return pg.Surface()
+
+
+def save2png(image: pg.Surface) -> bool:
+    """Save the image in .png format."""
+    return False
+
+
+if __name__ == "__main__":
+    pass
