dee37db795cf0833bffcf143a67cb8273030077f
Weisz Roland <weisz.roland@pohlen-dach.hu>
2022-11-17 07:17:01 +0100

    show a single line of text

--- gbfontmaker.py
+++ gbfontmaker.py
@@ -2,21 +2,49 @@
 
 
 import pygame as pg
+import pygame.freetype as freetype
 
 
-FONT_SIZE = 8  # font size in pixels
+FONT_SIZE = 16  # font size in pixels
 LAYOUT = (16, 14)   # layout of the font set per character (width, height)
 BACKGROUND_COLOR = "#e0f8cf"
+DEFAULT_FONT = "m5x7.ttf"
+DEFAULT_FONT_COLOR = "black"
 
 
-def make_image() -> pg.Surface:
+class Char(pg.sprite.Sprite):
+    def __init__(self, char, fonttype, color, pos):
+        super().__init__()
+        self._char = char
+        self._fonttype = fonttype
+        self._color = color
+        self.rect = self.image.get_rect(topleft=pos)
+
+    @property
+    def image(self):
+        return self._fonttype.render(self._text, self._color)[0]
+
+
+def make_image(font:str=DEFAULT_FONT,
+               size:int=FONT_SIZE,
+               fontcolor:str=DEFAULT_FONT_COLOR,
+               background:str=BACKGROUND_COLOR) -> pg.Surface:
     """Create a pygame.Surface object holding the characters."""
-    # steup the canvas
+    # setup the canvas
     canvas_width = FONT_SIZE * LAYOUT[0]
     canvas_height = FONT_SIZE * LAYOUT[1]
-    background_color = pg.Color(BACKGROUND_COLOR)
+    fontcolor = pg.Color(fontcolor)
+    background = pg.Color(background)
     canvas = pg.Surface((canvas_width, canvas_height))
-    canvas.fill(background_color)
+    canvas.fill(background)
+    
+    # setup font
+    fonttype = freetype.Font(font, size)
+
+    # draw a text
+    text, _ = fonttype.render("GB Studio Font Maker", fontcolor)
+    canvas.blit(text, (0, 0))
+
     return canvas
 
 
@@ -26,4 +54,6 @@ def save2png(image:pg.Surface) -> None:
 
 
 if __name__ == "__main__":
+    pg.init()
     save2png(make_image())
+    pg.quit()
